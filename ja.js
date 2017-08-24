bbeed670319f2ec490eca96bc5eda5916f776d87
anuj <anujlnmiit@gmail.com>
2017-08-25 01:22:17 +0530

    added random function,and all square becomes  same color when the answer is right

--- ja.js
+++ ja.js
@@ -13,25 +13,43 @@ var colors=[
 
 var col=["blue","green","yellow"];
 var square=document.getElementsByClassName("squ");
-var picked=colors[3];
+var t=ran();
+var picked=colors[t];
 var b=document.getElementById("pc");
 b.innerText=picked;
+var d=document.getElementById("r");
+
 for (var i=0;i<colors.length;i++) {
     square[i].style.background = colors[i];
     square[i].addEventListener("click", function () {
         var a=this.style.background;
         if(a==picked)
         {
+            d.innerText="CORRECT";
             alert("correct");
+            change(picked);
+
         }
         else
         {
+            d.innerText="TRY AGAIN";
             alert("wrong");
+            this.style.background="#232323";
+
         }
     });
 }
 
+function change(picked) {
+    for(var i=0;i<square.length;i++)
+    {
+        square[i].style.background=picked;
+    }
 
+}
+function  ran() {
+    return(Math.floor(Math.random()*colors.length))
+}
 // square[1].addEventListener("click",function () {
 //     alert(clicked);
 //
